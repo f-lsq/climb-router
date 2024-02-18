5fca1e13805fcd49c5af6f0e825e5fe5697cd597
Fiona Loo <fionaloo69@gmail.com>
2024-02-18 15:56:47 +0000

    Added boxicon CSS and changed leaflet tile layer

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Leaflet Map Javascript
-const coordinates = [1.3521, 103.8198]
+const coordinates = [1.3521, 103.8198] // SG coordinates (can set coordinates depending on user's location)
 
 const map = L.map('map').setView(coordinates, 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+	attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	minZoom: 0,
+	maxZoom: 22,
+	accessToken: 'Ks30FORufAmGLwXplyYzbHEuFVCd402Z2nze8jeves02tkNjZYaZZn9t2ybe2OkL'
 }).addTo(map);
