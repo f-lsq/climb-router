77fd941e884af3a9ed2b571c075bfdda85c43089
Fiona Loo <fionaloo69@gmail.com>
2024-02-22 02:26:37 +0000

    Added DOMContentLoaded

--- js-files/map.js
+++ js-files/map.js
@@ -1,10 +1,17 @@
 // Leaflet Map Javascript
-const coordinates = [1.3521, 103.8198] // SG coordinates (can set coordinates depending on user's location)
+document.addEventListener("DOMContentLoaded", function () {
+	
+	const coordinates = [1.3521, 103.8198] // SG coordinates (can set coordinates depending on user's location)
 
-const map = L.map('map').setView(coordinates, 13);
-L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
-	attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	minZoom: 0,
-	maxZoom: 22,
-	accessToken: 'Ks30FORufAmGLwXplyYzbHEuFVCd402Z2nze8jeves02tkNjZYaZZn9t2ybe2OkL'
-}).addTo(map);
+	const map = L.map('map').setView(coordinates, 13);
+	L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+		attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: 0,
+		maxZoom: 22,
+		accessToken: 'Ks30FORufAmGLwXplyYzbHEuFVCd402Z2nze8jeves02tkNjZYaZZn9t2ybe2OkL'
+	}).addTo(map);
+
+	let climbingGymMarkers = L.marker()
+})
+
+
