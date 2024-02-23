db1fde050ef6462edd93ab2297914c45ff267ef0
Fiona Loo <fionaloo69@gmail.com>
2024-02-23 05:45:30 +0000

    Added country codes

--- js-files/map.js
+++ js-files/map.js
@@ -1,8 +1,6 @@
 // Leaflet Map Javascript
-document.addEventListener("DOMContentLoaded", function () {
-	
-	const coordinates = [1.3521, 103.8198] // SG coordinates (can set coordinates depending on user's location)
 
+function createMap(coordinates){
 	const map = L.map('map').setView(coordinates, 13);
 	L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
 		attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -11,7 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		accessToken: 'Ks30FORufAmGLwXplyYzbHEuFVCd402Z2nze8jeves02tkNjZYaZZn9t2ybe2OkL'
 	}).addTo(map);
 
-	let climbingGymMarkers = L.marker()
-})
+	return map();
+}
+
 
 
