1404709128e30b2bddd2bd7c2d1fd92b26eea563
Fiona Loo <fionaloo69@gmail.com>
2024-02-28 18:23:25 +0000

    Added markers for climbing gyms and routes

--- js-files/data.js
+++ js-files/data.js
@@ -2,7 +2,6 @@ async function getCoordinates(map, locationCountry, locationType) {
   const response = await axios.get("json-files/routes.json");
   for (let eachLocation of response.data[locationCountry][locationType]) {
     const coordinates = eachLocation.metadata["parent-lnglat"].reverse();
-    const marker = L.marker([51.5, -0.09]).addTo(map);
-    // const marker = L.marker([coordinates]).addto(map);
+    const marker = L.marker(coordinates).addTo(map);
   }
 }
--- js-files/script.js
+++ js-files/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
-  sgCoordinates = [1.3521, 103.8198]
+  sgCoordinates = [1.3548, 103.7763]
   const map = createMap('map', sgCoordinates);
   console.log("map in script.js", map);
 
   getCoordinates(map, "singapore","climbing-gyms");
+  getCoordinates(map, "singapore","climbing-routes");
 })  
