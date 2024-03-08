64801869f629eb75b8922c9b226d5234b35451ae
Fiona Loo <fionaloo69@gmail.com>
2024-03-08 05:02:25 +0000

    Added foursquare API

--- js-files/data.js
+++ js-files/data.js
@@ -1,3 +1,6 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = 'fsq3cXgOhjFwVX0r0E7bNUOGt/eEKbKpC3i4URJqANeIoDQ=';
+
 /**
  * Get route and gym information from 'location.json'
  * @returns Array of routes and gyms in each country
@@ -16,3 +19,31 @@ async function getCountryData() {
   return response.data;
 }
 
+async function search(lat, lng, searchTerms) {
+  const response = await axios.get(`${BASE_API_URL}/places/search`, {
+    params: {
+      query: encodeURI(searchTerms), //encodeURI function to convert special characters to their encoded equivalent
+      ll: lat + "," + lng,
+      // categories: '13033', // enable use of categories
+      sort: 'DISTANCE',
+      radius: 2000,
+      limit: 50
+    },
+    headers: {
+      Accept: 'application/json',
+      Authorization: API_KEY
+    } 
+  }
+  )
+  return response.data;
+}
+
+async function getPhotoFromFourSquare(fsqid){
+  const response = await axios.get(`${BASE_API_URL}/places/${fsqid}/photos`, {
+    headers: {
+      Accept: 'application/json',
+      Authorisation: API_KEY
+    }
+  });
+  return response.data;
+}
